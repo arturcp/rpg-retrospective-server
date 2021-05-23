87ff032132ea6f057a803bcd54d3099c0cc5d985
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-23 16:36:35 -0300

    Add payload documentation

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const broadcast = (type, message) => {
 
 wsServer.on('request', (request) => {
   var userID = getUniqueID();
-  console.log((new Date()) + ' received a new connection from origin ' + request.origin + '.');
+
+  // console.log((new Date()) + ' received a new connection from origin ' + request.origin + '.');
 
   const connection = request.accept(null, request.origin);
   clients[userID] = connection;
@@ -38,16 +39,61 @@ wsServer.on('request', (request) => {
     console.log(message);
     if (message.type === 'utf8') {
       console.log('Received message: ', message.utf8Data);
+
       const payload = JSON.parse(message.utf8Data);
       const { type, value } = payload;
 
+      console.log('Message arrived: ', type);
+      console.log('message body: ', value)
+
       if (type === 'game-connection-request') {
+        /*
+          Payload's value example:
+
+          {
+            userID: 123123123123,
+            position: { x: 175, y: 433 },
+            direction: 2,
+            step: 1,
+            character: {
+              name: 'Gandalf',
+              type: 'male-wizard'
+            }
+          }
+        */
+
         // Save character in a list
-        players[value.userID] = value;
+        players[value.character.name] = value;
 
         // Broadcast list of characters to everyone
         broadcast('new-player', players);
       }
+
+      if (type === 'player-moved') {
+        /*
+          Payload's value example:
+
+          {
+            userID: 123123213,
+            position: { x: 10, y: 20 },
+            direction: 2,
+            step: 1,
+            character: {
+              name: 'Gandalf',
+              type: '',
+            }
+          }
+        */
+
+        players[value.characterName] = {
+          ...players[value.characterName],
+          position: value.position,
+          direction: value.direction,
+          step: value.step
+        }
+
+        broadcast('player-moved', players[value.characterName]);
+      }
     }
-  })
+  });
 });
