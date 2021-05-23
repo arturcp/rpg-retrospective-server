64aa18523a82045eec4d70b0a0ad067b158e7bf8
Artur Caliendo Prado <artur.prado@gmail.com>
2021-05-23 11:53:17 -0300

    Change server to work with messages

--- index.js
+++ index.js
@@ -11,11 +11,18 @@ const wsServer = new webSocketServer({
 });
 
 const clients = {}
+const players = {}
 
 const getUniqueID = () => {
   const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   return s4() + s4() + '-' + s4();
 };
+
+const broadcast = (type, message) => {
+  for(key in clients) {
+    clients[key].sendUTF(JSON.stringify({ message: message, type: type }));
+  }
+}
 
 wsServer.on('request', (request) => {
   var userID = getUniqueID();
@@ -24,14 +31,22 @@ wsServer.on('request', (request) => {
   const connection = request.accept(null, request.origin);
   clients[userID] = connection;
 
-  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+  // Send the UserID to the client when it connects in the backend
+  connection.sendUTF(JSON.stringify({ type: 'client-connected', userID: userID }));
 
   connection.on('message', (message) => {
+    console.log(message);
     if (message.type === 'utf8') {
       console.log('Received message: ', message.utf8Data);
+      const payload = JSON.parse(message.utf8Data);
+      const { type, value } = payload;
 
-      for(key in clients) {
-        clients[key].sendUTF(message.utf8Data);
+      if (type === 'game-connection-request') {
+        // Save character in a list
+        players[value.userID] = value;
+
+        // Broadcast list of characters to everyone
+        broadcast('new-player', players);
       }
     }
   })
