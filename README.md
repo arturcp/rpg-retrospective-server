3cfba63663d95c870e11d097cd3c32139f1b44e6
Artur Caliendo Prado <artur.prado@gmail.com>
2021-06-08 07:38:48 -0300

    Change lib that manages ws communication

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Resources
 
 - https://nodejs.org/en/knowledge/command-line/how-to-get-colors-on-the-command-line/
+
+- https://devcenter.heroku.com/articles/node-websockets
--- index.js
+++ index.js
@@ -2,27 +2,20 @@ const colors = require('colors');
 
 const express = require('express');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+const { Server } = require('ws');
+const wsServer = new Server({
+  port: 8000
+});
+
 const movementService = require('./player-moved');
 const quizService = require('./quiz-ready');
-
-const webSocketsServerPort = 8000;
-const webSocketServer = require('websocket').server;
-// const http = require('http');
-
-// const server = http.createServer();
-// server.listen(webSocketsServerPort);
-// console.log('listening on port 8000');
-
-const wsServer = new webSocketServer({
-  httpServer: server
-});
 
 // Clients contains the connections used by the websocket server
 // to communicate with all players. Each player has its own id,
@@ -75,87 +68,83 @@ const broadcast = (type, message) => {
   console.log(`Broadcasting ${type}...`);
   for(key in clients) {
     if (clients[key]) {
-      clients[key].sendUTF(JSON.stringify({ message: message, type: type }));
+      clients[key].send(JSON.stringify({ message: message, type: type }));
     }
   }
 }
 
-wsServer.on('request', (request) => {
+wsServer.on('connection', (connection) => {
   var userID = getUniqueID();
 
-  console.log((new Date()) + ' received a new connection from origin ' + request.origin + '.');
-
-  const connection = request.accept(null, request.origin);
+  // const connection = request.accept(null, request.origin);
   clients[userID] = connection;
 
   // Send the UserID to the client when it connects to the backend
-  connection.sendUTF(JSON.stringify({ type: 'client-connected', userID: userID }));
+  connection.send(JSON.stringify({ type: 'client-connected', userID: userID }));
 
   connection.on('message', (message) => {
     console.log('');
     console.log('===================================================================');
     console.log('');
 
-    if (message.type === 'utf8') {
-      console.log('                         MESSAGE RECEIVED'.yellow);
-      console.log('');
+    console.log('                         MESSAGE RECEIVED'.yellow);
+    console.log('');
 
-      const payload = JSON.parse(message.utf8Data);
-      const { type, value } = payload;
+    const payload = JSON.parse(message);
+    const { type, value } = payload;
 
-      console.log(`*  ${'Type'.bgCyan.black}: ${type}`);
-      console.log('* Body: ')
-      console.log(value);
-      console.log('');
+    console.log(`*  ${'Type'.bgCyan.black}: ${type}`);
+    console.log('* Body: ')
+    console.log(value);
+    console.log('');
 
-      if (type === 'players-list-request') {
-        broadcast('players-list', players);
-      }
-
-      // player has answered all the quizes
-      if (type === 'quiz-completed') {
-        broadcast('quiz-answered', value);
-      }
-
-      // the button to start the quiz was pressed
-      if (type === 'quiz-started') {
-        broadcast('quiz-started', players);
-      }
-
-      // The player has chosen a theme, its options, and the answer
-      if (type === 'quiz-ready') {
-        const response = quizService.quizReady(value, players);
-        broadcast('quiz-ready', value);
-      }
-
-      // The admin has completed the quiz and is sending the results to everyone
-      if (type === 'quiz-results-ready') {
-        broadcast('quiz-results', { value, playersList: players });
-      }
-
-      if (type === 'disconnect-player') {
-        clients[value.userID].sendUTF(JSON.stringify({ type: 'you-were-disconnected' }));
-        players[value.characterName] = null;
-        clients[value.userID].close();
-        broadcast('player-disconnected', players);
-      }
-
-      if (type === 'game-connection-request') {
-        // Save character in a list
-        players[value.character.name] = value;
-
-        // Broadcast list of characters to everyone
-        broadcast('new-player', players);
-        broadcast('players-list', players);
-      }
-
-      // Player has moved and needs to be updated in all screens.
-      if (type === 'player-moved') {
-        const response = movementService.playerMoved(value, players);
-        broadcast('player-moved', response);
-      }
-
-      console.log('===================================================================');
+    if (type === 'players-list-request') {
+      broadcast('players-list', players);
     }
+
+    // player has answered all the quizes
+    if (type === 'quiz-completed') {
+      broadcast('quiz-answered', value);
+    }
+
+    // the button to start the quiz was pressed
+    if (type === 'quiz-started') {
+      broadcast('quiz-started', players);
+    }
+
+    // The player has chosen a theme, its options, and the answer
+    if (type === 'quiz-ready') {
+      const response = quizService.quizReady(value, players);
+      broadcast('quiz-ready', value);
+    }
+
+    // The admin has completed the quiz and is sending the results to everyone
+    if (type === 'quiz-results-ready') {
+      broadcast('quiz-results', { value, playersList: players });
+    }
+
+    if (type === 'disconnect-player') {
+      clients[value.userID].send(JSON.stringify({ type: 'you-were-disconnected' }));
+      players[value.characterName] = null;
+      clients[value.userID].close();
+      broadcast('player-disconnected', players);
+    }
+
+    if (type === 'game-connection-request') {
+      // Save character in a list
+      players[value.character.name] = value;
+
+      // Broadcast list of characters to everyone
+      broadcast('new-player', players);
+      broadcast('players-list', players);
+    }
+
+    // Player has moved and needs to be updated in all screens.
+    if (type === 'player-moved') {
+      const response = movementService.playerMoved(value, players);
+      broadcast('player-moved', response);
+    }
+
+    console.log('===================================================================');
   });
 });
